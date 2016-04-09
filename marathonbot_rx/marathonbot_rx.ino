d97bb6d85d7a8eb6a72b9c20cc56704c617b2cce
Gui <me@guipinto.com>
2016-04-09 03:12:03 -0400

    New Arm Handler and general improvements for 2016 race!

--- marathonbot_rx/marathonbot_rx.ino
+++ marathonbot_rx/marathonbot_rx.ino
@@ -10,50 +10,42 @@
 
 #define ENGINE1_PIN  10
 #define THROTTLE_PIN 8
-#define STEARING_PIN 9
+#define STEERING_PIN 9
 #define SHUTDOWN_PIN  5
 #define I2C_SLAVE_ADDRESS 9
 
-#define escArmVal 89
-
 Servo throttle;
-Servo stearing;
+Servo steering;
 
 EasyTransfer ET; 
 EasyTransferI2C ETArm; 
 
 struct CONTROLLER_DATA_STRUCTURE{
-	int throttle;
-	int reverse;
-	int stearing;
-	boolean armESC;
-	boolean breaking;
-	boolean headlights;
-	boolean engine1;
-	boolean engine2;
-	int armM1;
-	int armM2;
-	int armM3;
-	int armM4;
+    int throttle;
+    int reverse;
+    int steering;
+    boolean breaking;
+    int armBase;
+    int armSholder;
+    int armForearm;
+    int armWrist; 
+    int armClaw; 
 };
 
 struct ARM_DATA_STRUCTURE{
-	int armM1;
-	int armM2;
-	int armM3;
-	int armM4;
+    int armBase;
+    int armSholder;
+    int armForearm;
+    int armWrist; 
+    int armClaw;
 };
 
 
 ARM_DATA_STRUCTURE arm;
 CONTROLLER_DATA_STRUCTURE controller;
 
-boolean engine1Fireing = false;
-unsigned long engine1FireingTime = 2000;
-unsigned long engine1FiredTime = 0;
-
 volatile boolean shutdown = false;
-
+int pos = 0; 
 void setup(){
   
 	Serial.begin(9600);
@@ -63,107 +55,72 @@ void setup(){
 	ET.begin(details(controller), &Serial);
 	ETArm.begin(details(arm), &Wire);
 	
-	stearing.attach(STEARING_PIN, 1000, 2000);
+	steering.attach(STEERING_PIN, 1000, 2000);
 	throttle.attach(THROTTLE_PIN, 1000, 2000);
 	
 	pinMode(13, OUTPUT);
 	
 	pinMode(THROTTLE_PIN, OUTPUT);
-	pinMode(STEARING_PIN, OUTPUT); 
+	pinMode(STEERING_PIN, OUTPUT); 
 	pinMode(ENGINE1_PIN, OUTPUT);  
 	pinMode(SHUTDOWN_PIN, OUTPUT);
 	
 	randomSeed(analogRead(0));
-
-	// Arm ESC
-	//throttle.write(89);
-	//delay(3000);
-
+  
 }
 
 int motorInit = 0;
 
 void loop(){
-	
-      if (!shutdown && digitalRead(SHUTDOWN_PIN) == HIGH) {
-          shutdown = true;
-      }
-      
-      if (shutdown) {
-        
-        stearing.write(127);
+
+/*
+    if (!shutdown && digitalRead(SHUTDOWN_PIN) == HIGH) {
+      shutdown = true;
+    }
+    
+    if (shutdown) {
+    
+        steering.write(127);
         throttle.write(90);
         
-        arm.armM1 = 0;
-        arm.armM2 = 0;
-        arm.armM3 = 0;
-        arm.armM4 = 0;           
-        
+        arm.armBase = -1;
+        arm.armSholder = -1;
+        arm.armForearm = -1;
+        arm.armWrist = -1;
+        arm.armClaw = -1;
         // And send to arm
-	ETArm.sendData(I2C_SLAVE_ADDRESS);
-		
+        ETArm.sendData(I2C_SLAVE_ADDRESS);
+        
         delay(18);
-         return; 
-      }
+        return; 
+    }
+*/
     
-  
-  
-	/*
-	I have all values i need: 
-	- arm with a value between 89 to 97 during 2s
-	- one direction:99 to 138 and the other 87 to 48
-	- brake but is too bad for the motor, i don´t use them in my project:0 and 254.
-	*/
-	
-	if(ET.receiveData()) {
-		
-		// Arming Electronic Speed Controller
-		if (controller.armESC) {
-			throttle.write( escArmVal );
-			delay(2000);
-		}
-		
-		// Stearing
-                int trim = 0;
-		stearing.write( map(controller.stearing, 0, 255, 20 + trim, 175 - trim) );
-		//stearing.write(controller.stearing);
 
-//-----Foward: 95-255
-//-----Reverse: 84-0
-
+    if(ET.receiveData()) {
+    			
+        // Steering
+        int steeringVal = controller.steering;
+        // Invert steering because servo is mounted backwards
+        steering.write(map(steeringVal, 0, 255, 255, 0));
+        
 		// Throttle
-		if (controller.breaking) {
-			throttle.write(88);
-		} else if (controller.reverse > 5) {
-			throttle.write( map(controller.reverse, 5, 255, 0, 84) );
-		} else if (controller.throttle > 5) {  
-			throttle.write( map(controller.throttle, 5, 255, 95, 255) );
-		} else {
-			throttle.write(90);
-		}	
-
-		// Rocket Engine
-		if (controller.engine1 && !engine1Fireing) {
-			engine1Fireing = true;
-			engine1FiredTime = millis();
-		}
-		if (engine1Fireing) {
-			if (millis() > engine1FiredTime + engine1FireingTime) {
-				digitalWrite(ENGINE1_PIN, LOW);
-				engine1Fireing = false;
-			} else {
-				digitalWrite(ENGINE1_PIN, HIGH);
-			}
-		}
-
-
-	
+        if (controller.breaking) {
+            throttle.write(0);
+        } else if (controller.reverse > 5) {
+            throttle.write( map(controller.reverse, 5, 255, 124, 0) );
+        } else if (controller.throttle > 5) {  
+            throttle.write( map(controller.throttle, 5, 255, 130, 255) );
+        } else {
+            throttle.write(127);
+        }	
 		
 		// Pass on arm values
-		arm.armM1 = controller.armM1;
-		arm.armM2 = controller.armM2;
-		arm.armM3 = controller.armM3;
-		arm.armM4 = controller.armM4;
+		arm.armBase = controller.armBase;
+		arm.armSholder = controller.armSholder;
+		arm.armForearm = controller.armForearm;
+		arm.armWrist = controller.armWrist;
+        arm.armClaw = controller.armClaw;
 	
 		// And send to arm
 		ETArm.sendData(I2C_SLAVE_ADDRESS);
--- marathonbot_tx/marathonbot_tx.ino
+++ marathonbot_tx/marathonbot_tx.ino
@@ -1,11 +1,34 @@
 #include <PS3USB.h>
 #include <EasyTransfer.h>
 
+#define DEBUG_PRINT false
+
 #define MODE_DRIVING  0
 #define MODE_ARM  1
 
 #define MAX_RESET 7 //MAX3421E pin 12
 #define MAX_GPX   8 //MAX3421E pin 17
+
+#define TRIM_MAX        100
+#define TRIM_DEFAULT    0
+#define TRIM_AMOUNT     5
+
+#define THROTTLE_IDLE     127
+
+#define ARM_BASE_MIN   50
+#define ARM_BASE_MAX   150
+#define ARM_SHOLDER_MIN   50
+#define ARM_SHOLDER_MAX   150
+#define ARM_FOREARM_MIN   50
+#define ARM_FOREARM_MAX   150
+#define ARM_WRIST_MIN   50
+#define ARM_WRIST_MAX   150
+#define ARM_CLAW_MIN   50
+#define ARM_CLAW_MAX   150
+
+#define LIGHT_MODE_IDLE        0
+#define LIGHT_MODE_TRIM_LEFT   2
+#define LIGHT_MODE_TRIM_RIGHT  3
 
 // Initialize USB Hooks
 USB Usb;
@@ -16,201 +39,279 @@ PS3USB PS3(&Usb); // This will just create the instance
 EasyTransfer ET; 
 
 struct SEND_DATA_STRUCTURE{
-	int throttle;
-	int reverse;
-	int stearing;
-	boolean armESC;
-	boolean breaking;
-	boolean headlights;
-	boolean engine1;
-	boolean engine2;
-	int armM1;
-	int armM2;
-	int armM3;
-	int armM4;  
+    int throttle = 0;
+    int reverse = 0;
+    int steering = 125;
+    boolean breaking = false;
+    int armBase = 90;
+    int armSholder = 90;
+    int armForearm = 90;
+    int armWrist = 90; 
+    int armClaw = 80; 
 };
+
 SEND_DATA_STRUCTURE rocketcontrols;
 
 volatile int mode = MODE_DRIVING;
 
+volatile int trim = TRIM_DEFAULT;
+volatile int controllerLightMode = LIGHT_MODE_IDLE;
+bool idleReset = false;
+
 void setup() {
   
-	// Stupid Sparkfun USB Shield..
-	pinMode(MAX_GPX, INPUT);
-	pinMode(MAX_RESET, OUTPUT);
-	digitalWrite(MAX_RESET, LOW);
-	delay(20);
-	digitalWrite(MAX_RESET, HIGH);
-	delay(20);
-	
-	pinMode(13, OUTPUT);
-	
-	// Initialize Serial and Easy Transfer
-	Serial.begin(9600);
-	delay(1000);
-	//Serial.println("Serial is ready.");
-	
-	// Initialize Easy-Transfer Lib
-	ET.begin(details(rocketcontrols), &Serial);
-	
-	// Initialize USB
-	if (Usb.Init() == -1) { // Halt!
-		Serial.println("USB.Init() did not start..");
-		while(Usb.Init() == -1) { 
-			Serial.println("Waiting on USB.init()...");
-			delay(500);
-		}
-	}
-	
-	//Serial.println("USB Initialized!");
-	
-	randomSeed(analogRead(0));
-	
-	modeChange(mode);
-	delay(200);		
+    // Stupid Sparkfun USB Shield..
+    pinMode(MAX_GPX, INPUT);
+    pinMode(MAX_RESET, OUTPUT);
+    digitalWrite(MAX_RESET, LOW);
+    delay(20);
+    digitalWrite(MAX_RESET, HIGH);
+    delay(20);
+    
+    pinMode(13, OUTPUT);
+    
+    // Initialize Serial and Easy Transfer
+    Serial.begin(9600);
+    delay(1000);
+    //Serial.println("Serial is ready.");
+    
+    // Initialize Easy-Transfer Lib
+    ET.begin(details(rocketcontrols), &Serial);
+    
+    // Initialize USB
+    if (Usb.Init() == -1) { // Halt!
+        if (DEBUG_PRINT) Serial.println("USB.Init() did not start..");
+        while(Usb.Init() == -1) { 
+            if (DEBUG_PRINT) Serial.println("Waiting on USB.init()...");
+            delay(500);
+        }
+    }
+    
+    if (DEBUG_PRINT) Serial.println("USB Initialized!");
+    
+    randomSeed(analogRead(0));
+    
+    modeChange(mode);
+    delay(200);     
 }
 
 void loop() {
 
-	Usb.Task();
-	
-	// Mode selection
-	if( PS3.getButtonClick(SELECT) ) {
-		
-		mode = mode == MODE_DRIVING ? MODE_ARM : MODE_DRIVING;
-	
-        	modeChange(mode); 
-	}
-	
-	switch(mode) {
-	case MODE_DRIVING: 
-		
-		// Stearing
-		rocketcontrols.stearing = int( PS3.getAnalogHat(LeftHatX) );
-		
-		// Throttle
-		if(PS3.getAnalogButton(R2)) {
-			rocketcontrols.throttle = int( PS3.getAnalogButton(R2) );
-		} else {
-			rocketcontrols.throttle = 0;
-		}
-		
-		// Reverse
-		if(PS3.getAnalogButton(L2)) {
-			rocketcontrols.reverse = int( PS3.getAnalogButton(L2) );
-		} else {
-			rocketcontrols.reverse = 0;
-		}
+    Usb.Task();
+    
+    // Mode selection
+    if( PS3.getButtonClick(SELECT) ) {
+        
+        mode = mode == MODE_DRIVING ? MODE_ARM : MODE_DRIVING;
+    
+        modeChange(mode); 
+    }
+    
+    switch(mode) {
+    case MODE_DRIVING: 
+        
+        // steering
+        rocketcontrols.steering = processSteering();
 
-		// Breaking
-		if(PS3.getButtonClick(CIRCLE)) {
-			rocketcontrols.breaking = true;    
-		} else {
-			rocketcontrols.breaking = false;  
-                }
-		
-		// Headlights
-		if(PS3.getButtonClick(L3)) {
-			rocketcontrols.headlights = !rocketcontrols.headlights;
-		}
-		
-		// Arm ESC
-		rocketcontrols.armESC = PS3.getButtonClick(START);
-		
-		// ENGINE 1  
-		rocketcontrols.engine1 = PS3.getButtonClick(L1) && PS3.getButtonClick(R1);
-		
-	break;
-	case MODE_ARM:
-	
-		int deadzone = 15;
-		
-		int sholderVal = armValue(int( PS3.getAnalogHat(LeftHatX) ), deadzone, true);
-		int armVal = armValue(int( PS3.getAnalogHat(LeftHatY) ), deadzone, true);
-		int forearmVal = armValue(int( PS3.getAnalogHat(RightHatY) ), deadzone, false);
-		//int handVal = armValue(int( PS3.getAnalogHat(RightHatX) ), deadzone, true);
-		
-                int handVal = 0;
-		if(PS3.getButtonPress(TRIANGLE)) {
-                    handVal = 255;
-		} else if (PS3.getButtonPress(CROSS)) {
-                    handVal = -255;
-                } else {
-                    handVal = 0;
-                }
+        // Steering Trim
+        if (PS3.getButtonClick(RIGHT)) {
+            adjustTrim(TRIM_AMOUNT);
+        } else if (PS3.getButtonClick(LEFT)) {
+            adjustTrim(-TRIM_AMOUNT);
+        }
+        
+        // Throttle
+        if(PS3.getAnalogButton(R2)) {
+            rocketcontrols.throttle = int( PS3.getAnalogButton(R2) );
+        } else {
+            rocketcontrols.throttle = 0;
+        }
+        
+        // Reverse
+        if(PS3.getAnalogButton(L2)) {
+            rocketcontrols.reverse = int( PS3.getAnalogButton(L2) );
+        } else {
+            rocketcontrols.reverse = 0;
+        }
 
-		rocketcontrols.armM1 = sholderVal;
-		rocketcontrols.armM2 = armVal;
-		rocketcontrols.armM3 = forearmVal;
-		rocketcontrols.armM4 = handVal;
+        // Breaking
+        if(PS3.getAnalogButton(CIRCLE)) {
+            rocketcontrols.breaking = true;    
+        } else {
+            rocketcontrols.breaking = false;  
+        }
+        
+        
+    break;
+    case MODE_ARM:
+    
+        int deadzone = 15;      
 
-	break;
-	}
-	
-/*
-	Serial.print("throttle=");
-	Serial.print(rocketcontrols.throttle);
-	Serial.print(", stearing=");
-	Serial.print(rocketcontrols.stearing);
-	Serial.print(", reverse=");
-	Serial.print(rocketcontrols.reverse);
-	Serial.print(", breaking=");
-	Serial.print(rocketcontrols.breaking);
-	Serial.print(", armM1=");
-	Serial.print(rocketcontrols.armM1);
-	Serial.print(", armM2=");
-	Serial.print(rocketcontrols.armM2);
-	Serial.print(", armM3=");
-	Serial.print(rocketcontrols.armM3);
-	Serial.print(", armM4=");
-	Serial.print(rocketcontrols.armM4);
-	Serial.println("");
-*/
-	ET.sendData();
-	
-	delay(50);
+        if(PS3.getAnalogButton(TRIANGLE)) {
+            rocketcontrols.armClaw = rocketcontrols.armClaw + 1;
+        } else if (PS3.getAnalogButton(CROSS)) {
+            rocketcontrols.armClaw = rocketcontrols.armClaw - 1;
+        }
+ 
+        rocketcontrols.armBase = controlDelta( rocketcontrols.armBase, int(PS3.getAnalogHat(LeftHatX)), deadzone, 5);
+        rocketcontrols.armSholder = controlDelta( rocketcontrols.armSholder, int(PS3.getAnalogHat(LeftHatY)), deadzone, 5);
+        rocketcontrols.armForearm = controlDelta( rocketcontrols.armForearm, int(PS3.getAnalogHat(RightHatY)), deadzone, 5);
+        rocketcontrols.armWrist = controlDelta( rocketcontrols.armWrist, int(PS3.getAnalogHat(RightHatX)), deadzone, 5);
+
+        // Apply Boundaries
+        rocketcontrols.armBase = constrain(rocketcontrols.armBase, ARM_BASE_MIN, ARM_BASE_MAX);
+        rocketcontrols.armSholder = constrain(rocketcontrols.armSholder, ARM_SHOLDER_MIN, ARM_SHOLDER_MAX);
+        rocketcontrols.armForearm = constrain(rocketcontrols.armForearm, ARM_FOREARM_MIN, ARM_FOREARM_MAX);
+        rocketcontrols.armWrist = constrain(rocketcontrols.armWrist, ARM_WRIST_MIN, ARM_WRIST_MAX);
+        rocketcontrols.armClaw = constrain(rocketcontrols.armClaw, ARM_CLAW_MIN, ARM_CLAW_MAX);
+
+    break;
+    }
+
+
+
+    if (DEBUG_PRINT) {
+        
+        Serial.print("throttle=");
+        Serial.print(rocketcontrols.throttle);
+        Serial.print(", steering=");
+        Serial.print(rocketcontrols.steering);
+        Serial.print(", reverse=");
+        Serial.print(rocketcontrols.reverse);
+        Serial.print(", breaking=");
+        Serial.print(rocketcontrols.breaking);
+        Serial.print(", armBase=");
+        Serial.print(rocketcontrols.armBase);
+        Serial.print(", armSholder=");
+        Serial.print(rocketcontrols.armSholder);
+        Serial.print(", armForearm=");
+        Serial.print(rocketcontrols.armForearm);
+        Serial.print(", armWrist=");
+        Serial.print(rocketcontrols.armWrist);
+        Serial.print(", armClaw=");
+        Serial.print(rocketcontrols.armClaw);
+        Serial.println("");
+        
+    } else {
+        
+      ET.sendData();
+        
+    }
+    
+    processControllerLights();
+
+    delay(30);
 }
 
 
 void modeChange(int mode) {
-	rocketcontrols.throttle = 0;
-	rocketcontrols.reverse = 0;
-	rocketcontrols.stearing = 125;
-	rocketcontrols.armESC = false;
-	rocketcontrols.breaking = false;
-	rocketcontrols.headlights = false;
-	rocketcontrols.engine1 = false;
-	rocketcontrols.engine1 = false;
-        rocketcontrols.armM1 = 0;
-	rocketcontrols.armM2 = 0;
-	rocketcontrols.armM3 = 0;
-	rocketcontrols.armM4 = 0;
+    rocketcontrols.throttle = 0;
+    rocketcontrols.reverse = 0;
+   // rocketcontrols.steering = 125;
+    rocketcontrols.breaking = false;
         
         //PS3.setRumbleOn(200, 100, 0, 0);
-	//PS3.setRumbleOn(RumbleHigh );
-	
-	if (mode == MODE_DRIVING) {
-		PS3.setLedOn(LED1);
-		PS3.setLedOff(LED2);
-		PS3.setLedOff(LED3);
-		PS3.setLedOn(LED4);
-	} else {
-		PS3.setLedOff(LED1);
-		PS3.setLedOn(LED2);
-		PS3.setLedOn(LED3);
-		PS3.setLedOff(LED4);
-	}
+    //PS3.setRumbleOn(RumbleHigh );
+    
 }
 
-int armValue(int val, int deadzone, boolean reverse) {
- 	if (val >= 125 + deadzone) {
-        if (reverse) return -( map( val, 125 + deadzone, 255, 0, 255) );
-		return map( val, 125 + deadzone, 255, 0, 255);
-	} else if (val <= 125 - deadzone) {
-		if (reverse) return map( val, 125 - deadzone, 0, 0, 255);
-		return -( map( val, 125 - deadzone, 0, 0, 255) );
-	} else {
-		return 0;
-	}
+int controlDelta(int sourceVal, int sourceControl, int deadzone, int maxSteps) {
+    int deltaVal = 0;
+    int calculatedVal = 0;
+    if (sourceControl >= 125 + deadzone) {
+        deltaVal = map(sourceControl, 128, 255, 1, maxSteps);
+        calculatedVal = sourceVal + deltaVal;
+    } else if (sourceControl <= 125 - deadzone) {
+        deltaVal = map(sourceControl, 0, 128, 1, maxSteps);
+        calculatedVal = sourceVal - deltaVal;
+    } else {
+        calculatedVal = sourceVal;
+    }
+    return calculatedVal;
+}
+
+
+void adjustTrim(int direction) {
+    trim = constrain(trim + direction, -(TRIM_MAX), TRIM_MAX);
+    if (direction > 0) {
+     controllerLightMode = LIGHT_MODE_TRIM_RIGHT;   
+    } else {
+     controllerLightMode = LIGHT_MODE_TRIM_LEFT;   
+    }
+}
+
+int processSteering() { 
+    int steering =  PS3.getAnalogHat(LeftHatX) + trim;      
+    steering = constrain(steering, 0, 255);
+    return steering;
+}
+
+int raceLightIndex = 0;
+int raceLightCounter = 0;
+int lightFireCounter = 0;
+int lightTrimCounter = 0;
+LEDEnum LEDs[4] = {LED4, LED3, LED2, LED1};
+void processControllerLights() {
+    
+    switch(controllerLightMode) {
+  case LIGHT_MODE_IDLE:
+
+    if (mode == MODE_DRIVING) {
+        
+         // Race Lights
+        raceLightCounter++;
+        if (raceLightCounter != 5) return;
+        raceLightCounter = 0;
+        for (int led = 0; led < 4; led++) {
+            if (led == raceLightIndex) {
+                PS3.setLedOn(LEDs[led]);
+            } else {
+                PS3.setLedOff(LEDs[led]);
+            }
+        }  
+        raceLightIndex++;
+        if (raceLightIndex > 3) raceLightIndex = 0;
+        
+    } else {
+
+        // Arm Indicator        
+        PS3.setLedOff(LED1);
+        PS3.setLedOn(LED2);
+        PS3.setLedOn(LED3);
+        PS3.setLedOff(LED4);
+    }
+
+    
+ break;
+  case LIGHT_MODE_TRIM_LEFT:
+  
+      PS3.setLedOn(LED4);
+      PS3.setLedOn(LED3);
+      PS3.setLedOff(LED2);
+      PS3.setLedOff(LED1);
+
+    lightTrimCounter++;
+    if (lightTrimCounter == 18) {
+         lightTrimCounter = 0;   
+        controllerLightMode = LIGHT_MODE_IDLE;
+    }
+
+ break;
+  case LIGHT_MODE_TRIM_RIGHT:
+  
+      PS3.setLedOff(LED4);
+      PS3.setLedOff(LED3);
+      PS3.setLedOn(LED2);
+      PS3.setLedOn(LED1);
+  
+    lightTrimCounter++;
+    if (lightTrimCounter == 18) {
+         lightTrimCounter = 0;   
+        controllerLightMode = LIGHT_MODE_IDLE;
+    }
+
+ break;
+    }
+    
 }
 
